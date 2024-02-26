b57385cefe75b8c39bae0a35c8b94d6138c2088f
calebestes <estes.caleb.b@gmail.com>
2024-02-26 14:33:14 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Group3_11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

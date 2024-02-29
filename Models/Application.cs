7e834ca8501c534322ed3553bc30bb28b53e176f
laurenlago <laurenlagomelo@gmail.com>
2024-02-29 14:57:31 -0700

    ok

---
+++ Models/Application.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Group3_11.Models
+{
+    public class Application
+
+    {
+        public int TaskId { get; set; }
+        public int Task { get; set; }
+        public int DueDate { get; set; }
+        public int Quadrant { get; set; }
+        public string Category { get; set; }
+        public bool Completed { get; set; }
+        
+    }
+}
---
+++ Models/ToDoListContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Group3_11.Models
+{
+    public class ToDoListContext: DbContext
+
+    {
+       public ToDoListContext(DbContextOptions<ToDoListContext>options) : base (options) 
+        {
+        }
+
+        public DbSet<Application> ToDoList { get; set; }
+        
+    }
+}
